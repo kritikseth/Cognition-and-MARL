e8003606d99f8f2a8a8c2d79dbce1f4e44e05a99
Kritik Seth <39276404+kritikseth@users.noreply.github.com>
2023-04-30 04:38:59 -0400

    interact v2

--- game.py
+++ game.py
@@ -1,3 +1,4 @@
+import sys
 import pygame
 from RushHour4.setup import Map
 from RushHour4.interact import Game
@@ -13,18 +14,18 @@ ROWS, COLS = 6, 9
 WINDOW_HEIGHT = blockSize * ROWS
 WINDOW_WIDTH = blockSize * COLS
 
-
 def main():
-    global SCREEN, CLOCK
+    global screen, CLOCK
     pygame.init()
-    SCREEN = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+    screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
     CLOCK = pygame.time.Clock()
-    SCREEN.fill(COLORS['Black'])
+    screen.fill(COLORS['Black'])
 
     map = Map(ROWS, COLS)
     obstruct_ids = [12, 13, 21, 30, 20, 29, 33, 34, 42, 43, 7, 8, 23]
     for position in obstruct_ids:
         map.obstruct(position, index=True)
+    
     game = Game(map)
     agents = {'a': 0, 'b': 45, '*': 35}
     game.initialize(agents)
@@ -40,22 +41,21 @@ def main():
 
 def drawGrid(grid):
     X, Y = 0, 0
-    for row in range(0, WINDOW_WIDTH, blockSize):
+    for row in range(0, WINDOW_HEIGHT, blockSize):
         Y = 0
-        for col in range(0, WINDOW_HEIGHT, blockSize):
-            rect = pygame.Rect(row, col, blockSize, blockSize)
+        for col in range(0, WINDOW_WIDTH, blockSize):
             print(X, Y)
+            rect = pygame.Rect(col, row, blockSize, blockSize)
             if grid[X][Y] == 'x':
-                pygame.draw.rect(SCREEN, COLORS['Black'], rect, 1)
-            # if grid[X][Y] == 'a':
-            #     pygame.draw.rect(SCREEN, COLORS['Blue'], rect, 1)
-            # if grid[X][Y] == 'b':
-            #     pygame.draw.rect(SCREEN, COLORS['Green'], rect, 1)
-            # if grid[X][Y] == '*':
-            #     pygame.draw.rect(SCREEN, COLORS['Red'], rect, 1)
-            # if grid[X][Y] == 'o':
-            else:
-                pygame.draw.rect(SCREEN, COLORS['White'], rect, 1)
+                pygame.draw.rect(screen, COLORS['Black'], rect, 1)
+            if grid[X][Y] == 'a':
+                pygame.draw.rect(screen, COLORS['Blue'], rect, 1)
+            if grid[X][Y] == 'b':
+                pygame.draw.rect(screen, COLORS['Green'], rect, 1)
+            if grid[X][Y] == '*':
+                pygame.draw.rect(screen, COLORS['Red'], rect, 1)
+            if grid[X][Y] == 'o':
+                pygame.draw.rect(screen, COLORS['White'], rect, 1)
             Y += 1
         X += 1
 
